f9049bf0eb476532c337a994b8c4810c69fee847
peaches <peizhe2001@hotmail.com>
2025-02-02 10:16:34 -0500

    Add driver modal with more information

--- components/DriverCard.tsx
+++ components/DriverCard.tsx
@@ -2,6 +2,15 @@
 
 import { useState } from "react";
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Driver } from "@/types/driver";
 import Image from "next/image";
 
@@ -17,33 +26,85 @@ export default function DriverCard({ driver }: DriverCardProps) {
 
   return (
     // TODO onclick open modal for the driver
-    <div
-      className="flex w-full flex-col justify-center rounded-lg align-middle"
-      style={{ backgroundColor: bgCol }}
-      onMouseEnter={() => setBgCol(`${teamColor}`)}
-      onMouseLeave={() => setBgCol("#F8F5F4")}
-    >
-      <div className="flex flex-row items-center gap-2 pb-2 align-middle">
+    <Dialog>
+      <DialogTrigger>
         <div
-          className="flex max-w-12 flex-col overflow-hidden rounded-lg"
-          style={{ backgroundColor: teamColor }}
+          className="flex w-full flex-col justify-center rounded-lg align-middle"
+          style={{ backgroundColor: bgCol }}
+          onMouseEnter={() => setBgCol(`${teamColor}`)}
+          onMouseLeave={() => setBgCol("#F8F5F4")}
         >
-          <Image
-            src={driverImg}
-            alt={driver.last_name}
-            width={1080}
-            height={1080}
-            className="object-cover"
-          />
+          <div className="flex flex-row items-center gap-2 pb-2 align-middle">
+            <div
+              className="flex max-w-12 flex-col overflow-hidden rounded-lg"
+              style={{ backgroundColor: teamColor }}
+            >
+              <Image
+                src={driverImg}
+                alt={driver.last_name}
+                width={1080}
+                height={1080}
+                className="object-cover"
+              />
+            </div>
+            <div className="font-nums text-darkRed ml-2 text-3xl font-bold">
+              {driver.driver_number}
+            </div>
+          </div>
+          <div className="text-darkRed ml-1 text-xl">{driver.first_name}</div>
+          <div className="text-darkRed ml-1 text-xl font-semibold">
+            {driver.last_name}
+          </div>
         </div>
-        <div className="ml-2 font-nums text-3xl font-bold text-darkRed">
-          {driver.driver_number}
-        </div>
-      </div>
-      <div className="ml-1 text-xl text-darkRed">{driver.first_name}</div>
-      <div className="ml-1 text-xl font-semibold text-darkRed">
-        {driver.last_name}
-      </div>
-    </div>
+      </DialogTrigger>
+      <DialogContent className="w-fit">
+        <DialogHeader>
+          <DialogTitle className="flex items-center gap-4 pb-4">
+            <div
+              className={`h-4 w-4 rounded-full`}
+              style={{ backgroundColor: teamColor }}
+            >
+              {" "}
+            </div>
+            {driver.full_name}
+          </DialogTitle>
+          <DialogDescription className="flex items-center gap-8">
+            <Image
+              src={driverImg}
+              alt={driver.last_name}
+              width={1080}
+              height={1080}
+              className="max-w-36 object-cover"
+            />
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <p>Broadcast Name</p>
+                <div className="text-darkRed indent-2 text-lg">
+                  {driver.broadcast_name}
+                </div>
+              </div>
+              <div>
+                <p>Country Code</p>
+                <div className="text-darkRed indent-2 text-lg">
+                  {driver.country_code}
+                </div>
+              </div>
+              <div>
+                <p>Team</p>
+                <div className="text-darkRed indent-2 text-lg">
+                  {driver.team_name}
+                </div>
+              </div>
+              <div>
+                <p className="">Driver Number</p>
+                <div className="text-darkRed indent-2 text-2xl font-bold">
+                  {driver.driver_number}
+                </div>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
