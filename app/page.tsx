a359d2d62dfdc78aa5526fd7850de098948a291a
peaches <peizhe2001@hotmail.com>
2025-02-01 20:30:14 -0500

    fix package-lock

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import WorldMap from "@/components/ui/world-map";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Container } from "@/components/Container";
 
 interface Circuit {
   circuitId: string;
@@ -47,22 +48,24 @@ export default function Home() {
       });
   }, []);
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <WorldMap dots={dots} />
+    <div className="flex flex-col items-center justify-items-center min-h-screen px-8 ">
+        <WorldMap dots={dots}/>
+      {/* <Container>
+      </Container> */}
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           <Image
             aria-hidden
             src="https://nextjs.org/icons/file.svg"
             alt="File icon"
             width={16}
             height={16}
-          />
+            />
           Learn
         </a>
         <a
@@ -70,14 +73,14 @@ export default function Home() {
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           <Image
             aria-hidden
             src="https://nextjs.org/icons/window.svg"
             alt="Window icon"
             width={16}
             height={16}
-          />
+            />
           Examples
         </a>
         <a
@@ -85,14 +88,14 @@ export default function Home() {
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           <Image
             aria-hidden
             src="https://nextjs.org/icons/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
-          />
+            />
           Go to nextjs.org →
           Go to nextjs.org →
         </a>
